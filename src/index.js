5b718e0cbf4b897cb5d11be0e5c6d72db0a83096
kx47g <kx47g2036@gmail.com>
2021-05-20 07:38:51 +0900

    クリックイベントの追加

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+document.addEventListener('click', (event) => {
+  const btn = document.getElementById('number-1');
+  btn.style.backgroundColor = "orange";
+});
+
 document.addEventListener('keydown', (event) => {
-  console.log(event.key);
-}); 
+  switch (event.key) {
+    case 'ArrowUp': {
+      const button = document.getElementById('number-2'); // DOM を取得する命令
+
+      button.style.top = `${Number(button.style.top.split('px')[0]) - 10}px`;
+
+      break;
+    }
+45
+    case 'ArrowDown': {
+      const button = document.getElementById('number-2'); // DOM を取得する命令
+
+      button.style.top = `${Number(button.style.top.split('px')[0]) + 10}px`;
+
+      break;
+    }
+
+    case 'ArrowRight': {
+      const button = document.getElementById('number-2'); // DOM を取得する命令
+
+      button.style.left = `${Number(button.style.left.split('px')[0]) + 10}px`;
+
+      break;
+    }
+
+    case 'ArrowLeft': {
+      const button = document.getElementById('number-2'); // DOM を取得する命令
+
+      button.style.left = `${Number(button.style.left.split('px')[0]) - 10}px`;
+
+      break;
+    }
+  }
+});
